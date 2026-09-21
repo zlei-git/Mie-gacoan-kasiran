72b7f7444c0001892d00cc8b8c027882445965e4
zlei-git <kerzvar@gmail.com>
2026-09-21 18:53:48 +0700

    debug: enable error display in api/index.php

--- api/index.php
+++ api/index.php
@@ -6,7 +6,9 @@ $_ENV['APP_KEY'] = 'base64:oyO9ojD/U2iNroCF0BHkP+U5TG0ULjMuMkQ5Mt7hon8=';
 $_SERVER['APP_KEY'] = 'base64:oyO9ojD/U2iNroCF0BHkP+U5TG0ULjMuMkQ5Mt7hon8=';
 
 putenv("APP_ENV=production");
-putenv("APP_DEBUG=false");
+putenv("APP_DEBUG=true");
+$_ENV['APP_DEBUG'] = 'true';
+$_SERVER['APP_DEBUG'] = 'true';
 putenv("CACHE_STORE=array");
 putenv("SESSION_DRIVER=cookie");
 putenv("LOG_CHANNEL=stderr");
@@ -39,5 +41,14 @@ $_ENV['DB_CONNECTION'] = 'sqlite';
 $_ENV['DB_DATABASE'] = $targetDb;
 $_SERVER['DB_CONNECTION'] = 'sqlite';
 $_SERVER['DB_DATABASE'] = $targetDb;
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Boot Exception: " . htmlspecialchars($e->getMessage()) . "</h1>";
+    echo "<p><b>File:</b> " . htmlspecialchars($e->getFile()) . " on line " . $e->getLine() . "</p>";
+    echo "<pre>" . htmlspecialchars($e->getTraceAsString()) . "</pre>";
+}
